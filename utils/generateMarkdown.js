f6307451c5f37f0f7de297ad86245bcf45914960
jtuvek <132848981+jtuvek@users.noreply.github.com>
2023-08-07 19:00:09 -0400

    init renderLicenseBadge function

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,6 +1,20 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (!license || license === 'No License') {
+    return '';
+}
+
+const licenseBadges = {
+  'MIT License': 'LICENSE_BADGE_URL',
+  'GNU GPLv3': 'LICENSE_BADGE_URL',
+  'Apache License 2.0': 'LICENSE_BADGE_URL',
+  'ISC License': 'LICENSE_BADGE_URL',
+};
+
+return `[![License](https://img.shields.io/badge/license-${license.replace(/ /g, '%20')}-${licenseBadges[license]}-blue.svg)]`;
+}
+
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
