ba6c7af16b227f41b43ec7ab5e1a5e3172f2627e
jtuvek <132848981+jtuvek@users.noreply.github.com>
2023-08-07 18:46:18 -0400

    added questions

--- index.js
+++ index.js
@@ -2,7 +2,47 @@
 const fs = require('fs');
 const inquirer = require('inquirer');
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+  {
+    type: 'input',
+    name: 'title',
+    message: 'Enter the title of your project:',
+  },
+  // Add more questions for other sections like description, installation, usage, etc.
+  {
+    type: 'input',
+    name: 'description',
+    message: 'Enter a description for your project:',
+  },
+  {
+    type: 'input',
+    name: 'installation',
+    message: 'Enter installation instructions:',
+  },
+  // ... (add more questions for other sections)
+  {
+    type: 'list',
+    name: 'license',
+    message: 'Choose a license for your project:',
+    choices: [
+      'MIT License',
+      'GNU GPLv3',
+      'Apache License 2.0',
+      'ISC License',
+      'No License',
+    ],
+  },
+  {
+    type: 'input',
+    name: 'github',
+    message: 'Enter your GitHub username:',
+  },
+  {
+    type: 'input',
+    name: 'email',
+    message: 'Enter your email address:',
+  },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
