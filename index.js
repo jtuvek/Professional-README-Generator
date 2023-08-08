53a6c3826e73c5109c3bd5363947add0f497a117
jtuvek <132848981+jtuvek@users.noreply.github.com>
2023-08-07 22:36:26 -0400

    added if/else to writeToFile and formatted doc

--- index.js
+++ index.js
@@ -47,7 +47,16 @@ const questions = [
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) { }
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) => {
+        if (err) {
+            console.error('Error writing README.md:', err);
+        } else {
+            console.log('README.md generated successfully!');
+        }
+    });
+}
+
 
 // TODO: Create a function to initialize app
 function init() {
